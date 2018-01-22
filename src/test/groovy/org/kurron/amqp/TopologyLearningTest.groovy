c7617f70e4b0adb069fcba4a07f85bd9dfdfd818
Ron Kurr <kurr@kurron.org>
2018-01-22 18:23:46 -0500

    Trying to figure out how to build the topology.

--- src/test/groovy/org/kurron/amqp/TopologyLearningTest.groovy
+++ src/test/groovy/org/kurron/amqp/TopologyLearningTest.groovy
@@ -1,15 +1,17 @@
 package org.kurron.amqp
 
+import java.util.concurrent.ThreadLocalRandom
+
 class TopologyLearningTest {
 
     static void main(String[] args) {
 
-        def nodeCount = 28
+        def nodeCount = 12
         if ( 0 != nodeCount % 4 ) {
             throw new IllegalArgumentException( 'Node count must be multiple of 4' )
         }
-        int oneQuarter = nodeCount / 4
-        int oneHalf = nodeCount / 2
+        int oneQuarter = nodeCount.intdiv( 4 ).intValue()
+        int oneHalf = nodeCount.intdiv( 2 ).intValue()
         def nodes = (1..nodeCount).collect { new Node( it as String ) }
         def bottomTier = (1..oneQuarter).collect { nodes.pop() }.sort()
         def middleTier = (1..oneHalf).collect { nodes.pop() }.sort()
@@ -18,10 +20,28 @@ class TopologyLearningTest {
         println "topTier is ${topTier}"
         println "middleTier is ${middleTier}"
         println "bottomTier is ${bottomTier}"
+
+        // top node gets 1..N middle nodes
+        // middle node gets 1..N bottom nodes
+
+        topTier.each { top->
+            def numberToAdd = ThreadLocalRandom.current().nextInt( middleTier.size() ) + 1
+            numberToAdd.times {
+                top.outbound.add( middleTier.get( ThreadLocalRandom.current().nextInt( middleTier.size() ) ) )
+            }
+        }
+        middleTier.each { middle->
+            def numberToAdd = ThreadLocalRandom.current().nextInt( bottomTier.size() ) + 1
+            numberToAdd.times {
+                middle.outbound.add( bottomTier.get( ThreadLocalRandom.current().nextInt( bottomTier.size() ) ) )
+            }
+        }
+        'foo'
     }
 
-    static class Node {
+    static class Node implements Comparable<Node>{
         final String label
+        final SortedSet<Node> outbound = new TreeSet<>()
 
         Node(String label) {
             this.label = label
@@ -30,6 +50,11 @@ class TopologyLearningTest {
         @Override
         String toString() {
             label
+        }
+
+        @Override
+        int compareTo(Node o) {
+            label.compareTo( o.label )
         }
     }
 }
