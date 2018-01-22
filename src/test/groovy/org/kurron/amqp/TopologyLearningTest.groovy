b114a54e3cff42d0d2eb972ca319e92f3e28eb9d
Ron Kurr <kurr@kurron.org>
2018-01-22 18:06:56 -0500

    Trying to figure out how to build the topology.

--- src/test/groovy/org/kurron/amqp/TopologyLearningTest.groovy
+++ src/test/groovy/org/kurron/amqp/TopologyLearningTest.groovy
@@ -4,14 +4,32 @@ class TopologyLearningTest {
 
     static void main(String[] args) {
 
-        def rawNodeCount = 49
-        def nodeCount = rawNodeCount < 4 ? 4 : 0 == rawNodeCount % 2 ? rawNodeCount : rawNodeCount + 1
+        def nodeCount = 28
+        if ( 0 != nodeCount % 4 ) {
+            throw new IllegalArgumentException( 'Node count must be multiple of 4' )
+        }
         int oneQuarter = nodeCount / 4
         int oneHalf = nodeCount / 2
-        def nodes = (1..nodeCount).collect { it }
-        def topTier = nodes.take( oneQuarter )
-        def middleTier = nodes.take( oneHalf )
-        def bottomTier = nodes.take( oneQuarter )
+        def nodes = (1..nodeCount).collect { new Node( it as String ) }
+        def bottomTier = (1..oneQuarter).collect { nodes.pop() }.sort()
+        def middleTier = (1..oneHalf).collect { nodes.pop() }.sort()
+        def topTier = (1..oneQuarter).collect { nodes.pop() }.sort()
         println "Node count is ${nodeCount}"
+        println "topTier is ${topTier}"
+        println "middleTier is ${middleTier}"
+        println "bottomTier is ${bottomTier}"
+    }
+
+    static class Node {
+        final String label
+
+        Node(String label) {
+            this.label = label
+        }
+
+        @Override
+        String toString() {
+            label
+        }
     }
 }
